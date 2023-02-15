56c324a7956df05c858d641b6c19260a2a055370
Lee GunJun <ac9832@naver.com>
2023-02-15 23:07:18 +0900

    ejs 템플릿으로 변경 및 업데이트

--- testjs/app.js
+++ testjs/app.js
@@ -1,17 +1,21 @@
 const path = require('path')
 const http = require('http')
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressHbs = require('express-handlebars')
 
 const app = express()
 
-app.set('view engine', 'pug')
+// app.engine('hbs', expressHbs({
+//   layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'
+// }))
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
@@ -19,7 +23,7 @@ app.use('/admin', adminRoutes.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
 
 app.listen(3000)
--- testjs/routes/shop.js
+++ testjs/routes/shop.js
@@ -8,7 +8,14 @@ const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
   const products = adminData.products
-  res.render('shop', {prods: products, docTitle: 'Shop' })
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCss: true
+  })
 })
 
 module.exports = router
