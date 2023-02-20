d0525ded44769901c78d7463591a19ff397ece18
Lee GunJun <ac9832@naver.com>
2023-02-20 23:23:21 +0900

    mvc 적용 및 Model 추가
    파일에 데이터 저장 기능 추가

--- testjs/app.js
+++ testjs/app.js
@@ -3,7 +3,8 @@ const http = require('http')
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const expressHbs = require('express-handlebars')
+
+const errorController = require('./controllers/error')
 
 const app = express()
 
@@ -15,15 +16,14 @@ app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.use('/admin', adminRoutes.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' })
-})
+app.use(errorController.get404)
 
 app.listen(3000)
--- testjs/routes/shop.js
+++ testjs/routes/shop.js
@@ -1,21 +1,10 @@
 const path = require('path')
 
 const express = require('express')
-const rootDir = require('../util/path')
+const productysController = require('../controllers/products')
 
 const router = express.Router()
-const adminData = require('./admin')
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCss: true
-  })
-})
+router.get('/', productysController.getProducts)
 
 module.exports = router
